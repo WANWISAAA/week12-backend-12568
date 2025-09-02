0376aaa98e8ed05d5066abbdc97f273c00ca2217
WANWISAAA <chumpornw6@gmail.com>
2025-09-02 12:25:30 +0700

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,94 @@
-<template>
-  <div class="text-center text-purple-500 font-semibold my-4">
-    <div class="flex justify-center items-center">
-      <svg class="text-highlighted block w-auto h-6" width="800" height="200" viewBox="0 0 800 200" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M377 200C379.16 200 381 198.209 381 196V103C381 103 386 112 395 127L434 194C435.785 197.74 439.744 200 443 200H470V50H443C441.202 50 439 51.4941 439 54V148L421 116L385 55C383.248 51.8912 379.479 50 376 50H350V200H377Z" fill="currentColor"></path><path d="M726 92H739C742.314 92 745 89.3137 745 86V60H773V92H800V116H773V159C773 169.5 778.057 174 787 174H800V200H783C759.948 200 745 185.071 745 160V116H726V92Z" fill="currentColor"></path><path d="M591 92V154C591 168.004 585.742 179.809 578 188C570.258 196.191 559.566 200 545 200C530.434 200 518.742 196.191 511 188C503.389 179.809 498 168.004 498 154V92H514C517.412 92 520.769 92.622 523 95C525.231 97.2459 526 98.5652 526 102V154C526 162.059 526.457 167.037 530 171C533.543 174.831 537.914 176 545 176C552.217 176 555.457 174.831 559 171C562.543 167.037 563 162.059 563 154V102C563 98.5652 563.769 96.378 566 94C567.96 91.9107 570.028 91.9599 573 92C573.411 92.0055 574.586 92 575 92H591Z" fill="currentColor"></path><path d="M676 144L710 92H684C680.723 92 677.812 93.1758 676 96L660 120L645 97C643.188 94.1758 639.277 92 636 92H611L645 143L608 200H634C637.25 200 640.182 196.787 642 194L660 167L679 195C680.818 197.787 683.75 200 687 200H713L676 144Z" fill="currentColor"></path><path d="M168 200H279C282.542 200 285.932 198.756 289 197C292.068 195.244 295.23 193.041 297 190C298.77 186.959 300.002 183.51 300 179.999C299.998 176.488 298.773 173.04 297 170.001L222 41C220.23 37.96 218.067 35.7552 215 34C211.933 32.2448 207.542 31 204 31C200.458 31 197.067 32.2448 194 34C190.933 35.7552 188.77 37.96 187 41L168 74L130 9.99764C128.228 6.95784 126.068 3.75491 123 2C119.932 0.245087 116.542 0 113 0C109.458 0 106.068 0.245087 103 2C99.9323 3.75491 96.7717 6.95784 95 9.99764L2 170.001C0.226979 173.04 0.00154312 176.488 1.90993e-06 179.999C-0.0015393 183.51 0.229648 186.959 2 190C3.77035 193.04 6.93245 195.244 10 197C13.0675 198.756 16.4578 200 20 200H90C117.737 200 137.925 187.558 152 164L186 105L204 74L259 168H186L168 200ZM89 168H40L113 42L150 105L125.491 147.725C116.144 163.01 105.488 168 89 168Z" fill="#00DC82"></path></svg>
-    </div>
-    <p class="my-4">Home</p>
-  </div>
-</template>
-
-
 <script lang="ts" setup>
-
+  const title = ref('วิทยาลัยเทคนิคตราด')
 </script>
+
+<template>
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="title" />
+    <Style>
+    body { background-color: green; }
+    </Style>
+  </Head>
+
+  <div class="font-[Sarabun] bg-white py-14 px-6 sm:px-12 bg-gradient-to-r from-pink-50 via-pink-50 to-blue-100">
+    <div class="max-w-screen-xl mx-auto shadow-2xl rounded-xl p-4">
+      <div class="max-w-screen-md font-[Kanit]">
+          <h2 class="text-[#801810] xl:text-5xl md:text-5xl text-4xl font-bold !leading-tight">สาส์นจากผู้อำนวยการ</h2>
+      </div>
+
+      <div class="grid lg:grid-cols-2 gap-16 lg:gap-12 mt-6">
+        <div>
+          <p class="text-slate-600 md:text-md leading-relaxed mx-auto max-w-screen-xl">
+            ขอขอบคุณผู้ปกครองนักเรียน นักศึกษาที่ให้ความไว้วางใจวิทยาลัย ที่ให้ความร่วมมืออย่างดีในกิจกรรมต่างๆ ของวิทยาลัยฯ
+            ขอขอบใจนักเรียนนักศึกษาทุกคนที่ปฏิบัติตนอยู่ในระเบียบของวิทยาลัย อยู่ในโอวาทของทุกคน เป็นนักเรียนนักศึกษาที่ วิทยาลัยภาคภูมิใจ.
+          </p>
+
+          <div class="max-w-screen-md mt-2">
+            <h2 class="xl:text-xl md:text-xl text-xl font-sm !leading-tight text-[#801810]">
+              “เทคนิคตราดโฉมใหม่ ใส่ใจบุตรหลาน จัดการศึกษา เพื่อการมีงานทำ”</h2>
+          </div>
+
+          <div class="mt-12 flex gap-6 items-center flex-wrap">
+            <button type='button' class="bg-[#55F5A3] hover:bg-green-400 transition-all text-slate-900 font-semibold text-[15px] rounded-full px-6 py-3 cursor-pointer">Getting
+              Started
+            </button>
+            <a href="javascript:void(0)" class="text-slate-900 text-[15px] font-semibold underline max-sm:block whitespace-nowrap">API
+              Documentation
+            </a>
+          </div>
+
+          <div class="mt-12 flex flex-wrap gap-x-2 gap-y-2">
+            <a href="https://rms.trattc.ac.th/?p=login" target="_blank">
+              <img src="~/assets/img/rms.png" class="shrink-0 w-48 h-32 shadow-2xl rounded-md p-2 hover:scale-[1.05] transition-all" alt="brand-logo1" />
+            </a>
+
+            <a href="https://std2018.vec.go.th/web/" target="_blank">
+              <img src="~/assets/img/std02.png" class="w-48 h-32 shrink-0 shadow-2xl rounded-md p-2 hover:scale-[1.05] transition-all" alt="brand-logo2" />
+            </a>
+
+            <a href="https://admission.vec.go.th/web/Login.htm?mode=index" target="_blank">
+              <img src="~/assets/img/std_register.png" class="w-48 h-32 shrink-0 shadow-2xl rounded-md p-2 hover:scale-[1.05] transition-all" alt="brand-logo3" />
+            </a>
+            </div>
+        </div>
+
+        <div class="max-w-full">
+            <img src="~/assets/img/child2568.jpg" class="shrink-0 shadow-2xl rounded-md object-contain hover:scale-[1.05] transition-all" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="font-[Sarabun] min-h-screen bg-white px-6 sm:px-12 bg-gradient-to-r from-pink-50 via-pink-50 to-blue-100 py-2 mb-10">
+    <div class="mx-auto sm:max-w-6xl md:max-w-7xl max-w-md pl-0 md:pl-0 shadow-2xl rounded-xl">
+      <h2 class="text-2xl sm:text-3xl font-bold text-slate-900 mb-2 sm:mb-2 mt-5 p-4">ข่าวล่าสด</h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-4 p-4">
+
+        <div class="bg-white flex flex-col rounded-lg overflow-hidden shadow-md hover:scale-[1.02] transition-all">
+          <div class="aspect-[3/2] cursor-pointer">
+            <img src="https://readymadeui.com/Imagination.webp" class="w-full h-full object-cover" alt="Card image" />
+          </div>
+
+          <div class="p-6">
+            <h3 class="text-slate-900 text-xl font-semibold">Heading</h3>
+            <p class="mt-3 text-md text-slate-500 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sunt consequuntur necessitatibus, eveniet
+              eligendi ullam?
+            </p>
+            <button type="button" class="mt-6 px-5 py-2.5 rounded-lg text-white text-sm font-medium tracking-wider border-none outline-none bg-blue-600 
+            hover:bg-blue-700 active:bg-blue-600 cursor-pointer">
+              View
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</template>
 
 <style>
 
